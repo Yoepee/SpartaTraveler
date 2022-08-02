d3ce645903dea528b5a4557d972545b00f5aad12
sun2525 <ghd7673@naver.com>
2022-08-03 02:05:01 +0900

    full fix

--- post-upload/app.py
+++ post-upload/app.py
@@ -52,7 +52,7 @@ def save_diary():
 
     db.diary.insert_one(doc)
 
-    return jsonify({'msg': '저장 완료!'})
+    return jsonify({'msg': '저장 완료!'})+home()
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
