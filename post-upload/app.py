50f220c7a62488fddac7e7e6045dabd677b263fc
sun2525 <ghd7673@naver.com>
2022-08-03 10:14:11 +0900

    full function completion

--- post-upload/app.py
+++ post-upload/app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient
-# client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb+srv://test:1234@cluster0.sh1ym.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta_plus_week1
 
@@ -15,14 +14,14 @@ def home():
 
 @app.route('/main')
 def main():
-    return render_template('index.html')
+    return render_template('main_test.html')
 
-@app.route('/diary', methods=['GET'])
+@app.route('/view', methods=['GET'])
 def show_diary():
     diaries = list(db.diary.find({}, {'_id': False}))
     return jsonify({'all_diary': diaries})
 
-@app.route('/diary', methods=['POST'])
+@app.route('/posting', methods=['POST'])
 def save_diary():
     title_receive = request.form['title_give']
     content_receive = request.form['content_give']
@@ -52,7 +51,7 @@ def save_diary():
 
     db.diary.insert_one(doc)
 
-    return jsonify({'msg': '저장 완료!'})+home()
+    return jsonify({'msg': '저장 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
