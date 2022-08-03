d4b0c8caf339396d36a3f476f7c283e5c2b4f334
sun2525 <ghd7673@naver.com>
2022-08-04 00:29:48 +0900

    add main_post_page

--- post-upload/app.py
+++ post-upload/app.py
@@ -9,6 +9,7 @@ db = client.dbsparta_plus_week1
 from datetime import datetime
 
 from bson.json_util import dumps
+from bson.objectid import ObjectId
 
 @app.route('/')
 def home():
@@ -18,13 +19,22 @@ def home():
 def main():
     return render_template('main_test.html')
 
-#검색 기능
-@app.route('/view', methods=['GET'])
-def show_diary():
-    diaries = list(db.diary.find({}, {'_id': False}))
-    return jsonify({'all_diary': diaries})
+#상세페이지를 보여주는 기능
+@app.route('/post', methods=['GET'])
+def post():
+    file_name = request.args.get('file_name_give', False)
+    print(file_name)
+    return render_template('post.html', value=file_name)
 
-#데이터베이스에 저장된 게시글을 전송해 main_test.html파일에서 보여줌
+@app.route('/showpost', methods=['POST'])
+def show_post():
+    file_name = request.form.get('file_name_give', False)
+    print(file_name)
+    diaries = list(db.diary.find({'file': file_name}))
+    print(dumps(diaries))
+    return jsonify({'all_diary': dumps(diaries)})
+
+#검색 기능
 @app.route('/search', methods=['POST'])
 def search_show():
     find_name = request.form.get('find_name_give', False)
@@ -33,6 +43,12 @@ def search_show():
 
     diaries = list(db.diary.find({find_name: {'$regex':search_name}}))
     return jsonify({'all_diary': dumps(diaries)})
+
+#데이터베이스에 저장된 게시글을 전송해 main_test.html파일에서 보여줌
+@app.route('/view', methods=['GET'])
+def show_diary():
+    diaries = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'all_diary': diaries})
 
 #사용자로부터 게시글 데이터가 들어오면 데이터베이스에 저장
 @app.route('/posting', methods=['POST'])
