465d06bfd812a1c8021e99edeef17f5c413a631a
Yoepee <kdy2975@naver.com>
2022-08-03 10:52:51 +0900

    #8 템플릿 연결 로그인, 회원가입, 게시판, 글 작성, 상세페이지 템플릿 연결 https://github.com/SeongeunYang/SarangBang/blob/main/templates/layout_postview.html 에서 css 받아와서 상세페이지 적용

--- first-mini/app.py
+++ first-mini/app.py
@@ -5,7 +5,7 @@ import hashlib
 # flask 의 기본내장함수들 = render_templaste : html 위치, request = 정보 받아오기, jsonify = 메세지보내기, session = 서버에 session값 등 저장
 from flask import *
 # pyjwt를 호출
-import jwt
+import jwt, re
 
 from datetime import datetime, timedelta, date
 
@@ -59,6 +59,10 @@ def home1():
 #한칸을 더 내리면 안됨
 @app.route('/login')
 def login():
+    token_receive = request.cookies.get('mytoken')
+    if token_receive is not None:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms='HS256')
+
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
@@ -69,10 +73,22 @@ def join():
     return render_template('join.html')
 
 
-# href='/'사용이 안먹을 때 html파일로 바로 연결되도록 등록
+# 글 작성시 token을 확인하여 넘겨주기 위한 내용
 @app.route('/write')
 def home2():
-    return render_template('write.html')
+    token_receive = request.cookies.get('mytoken')
+    # 토큰 유효성검사
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms='HS256')
+        user_info = db.users.find_one({'userid': payload['id']})
+        return render_template('write.html', id=user_info['userid'])
+    # 토큰의 유효기간이 만료되었다는 에러문구
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    # 토큰이 유효하지 않다는 에러문구
+    except jwt.exceptions.DecodeError:
+        # return jsonify({'result': 'fail', 'msg': "로그인이 필요합니다!"})
+        return redirect(url_for("login", msg="로그인을 하지않으면 글을 작성할 수 없습니다."))
 
 
 # 회원가입
@@ -82,29 +98,40 @@ def join_ok():
     id_receive = request.form['id_give']
     password_receive = request.form['password_give']
     password2_receive = request.form['password2_give']
+    name_receive = request.form['name_give']
+
+    reg = "^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$"
 
     # 공백이 있는지 유효성검사
-    if id_receive == "" or password_receive == "" or password2_receive == "":
-        return jsonify({'msg': '작성되지 않은 정보가 있습니다.'})
+    if id_receive == "" or password_receive == "" or password2_receive == "" or name_receive == "":
+        return jsonify({'result': 'fail', 'msg': '작성되지 않은 정보가 있습니다.'})
     else:
-        # 중복 아이디가 있는지 유효성 검사
-        if (db.users.find_one({'userid': id_receive})) is not None:
-            return jsonify({'msg': '중복되는 아이디가 있습니다.'})
-        else:
-            # 비밀번호는 잘못입력하면 계정을 못쓸 수 있기때문에 1,2를 통해 검사함 (동일한지 유효성검사)
-            if password_receive != password2_receive:
-                return jsonify({'msg': '비밀번호가 다릅니다.'})
+        # 비밀번호 유효성 검사
+        if bool(re.match(reg, password_receive)):
+            # 중복 아이디가 있는지 유효성 검사
+            if (db.users.find_one({'userid': id_receive})) is not None:
+                return jsonify({'result': 'fail', 'msg': '중복되는 아이디가 있습니다.'})
             else:
-                # pw 암호화 (hexdigest = 16진수로 암호화)
-                pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-                # DB에 저장될 딕셔너리형 자료형으로 대입
-                doc = {
-                    'userid': id_receive,
-                    'password': pw_hash
-                }
-                # 데이터베이스 저장
-                db.users.insert_one(doc)
-                return jsonify({'msg': '회원가입 완료'})
+                if (db.users.find_one({'name': name_receive})) is not None:
+                    return jsonify({'result': 'fail', 'msg': '중복되는 닉네임이 있습니다.'})
+                    # 비밀번호는 잘못입력하면 계정을 못쓸 수 있기때문에 1,2를 통해 검사함 (동일한지 유효성검사)
+                else:
+                    if password_receive != password2_receive:
+                        return jsonify({'result': 'fail', 'msg': '비밀번호가 일치하지 않습니다.'})
+                    else:
+                        # pw 암호화 (hexdigest = 16진수로 암호화)
+                        pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+                        # DB에 저장될 딕셔너리형 자료형으로 대입
+                    doc = {
+                        'userid': id_receive,
+                        'password': pw_hash,
+                        'name': name_receive
+                    }
+                    # 데이터베이스 저장
+                    db.users.insert_one(doc)
+                    return jsonify({'result': 'success', 'msg': '회원가입 완료'})
+        else:
+            return jsonify({'result': 'fail', 'msg': '비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자'})
 
 
 # 로그인 되는지 확인하는 함수
@@ -148,6 +175,10 @@ def login_ok():
 
 @app.route('/write2', methods=['POST'])
 def save_post():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms='HS256')
+    user_info = db.users.find_one({'userid': payload['id']})
+
 
     title_receive = request.form['title_give']
     star_receive = request.form['star_give']
@@ -162,7 +193,7 @@ def save_post():
     mydate = today.strftime('%y.%m.%d')
     filename = f'file-{mytime}'
 
-    save_to = f'static/{filename}.{extension}'
+    save_to = f'static/img/{filename}.{extension}'
     file.save(save_to)
 
     doc = {
@@ -171,7 +202,8 @@ def save_post():
         'address': address_receive,
         'content': content_receive,
         'file': f'{filename}.{extension}',
-        'date': mydate
+        'date': mydate,
+        'name' : user_info['name']
     }
 
     db.posts.insert_one(doc)
@@ -179,10 +211,50 @@ def save_post():
     return jsonify({'msg': '저장완료'})
 
 
+@app.route('/checking', methods=['POST'])
+def id_check():
+    id_receive = request.form['id_give']
+    user = db.users.find_one({'userid': id_receive})
+    reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+    if id_receive == "":
+        return jsonify({'result': 'fail', 'msg': '이메일을 입력해주세요'})
+    else:
+        if bool(re.match(reg, id_receive)):
+            if user is None:
+                return jsonify({'result': 'success', 'msg': "사용할 수 있는 이메일입니다."})
+            else:
+                return jsonify({'result': 'fail', 'msg': '이미 가입된 이메일입니다.'})
+        else:
+            return jsonify({'result': 'fail', 'msg': '이메일 형식을 맞춰주세요.'})
+
+
+@app.route('/checking2', methods=['POST'])
+def name_check():
+    name_receive = request.form['name_give']
+    user = db.users.find_one({'name': name_receive})
+    reg = "^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]){1,10}"
+
+    if name_receive == "":
+        return jsonify({'result': 'fail', 'msg': '닉네임을 입력해주세요'})
+    else:
+        if bool(re.match(reg, name_receive)):
+            if user is None:
+                return jsonify({'result': 'success', 'msg': "사용할 수 있는 닉네임입니다."})
+            else:
+                return jsonify({'result': 'fail', 'msg': '이미 가입된 닉네임입니다.'})
+        else:
+            return jsonify({'result': 'fail', 'msg': '닉네임 형식을 맞춰주세요.'})
+
+
 @app.route('/write2', methods=['GET'])
 def show_diary():
     posts = list(db.posts.find({}, {'_id': False}))
     return jsonify({'all_posts': posts})
+
+
+@app.route('/detail', methods=['GET'])
+def show_detail():
+    return render_template('detail.html')
 
 
 if __name__ == '__main__':
