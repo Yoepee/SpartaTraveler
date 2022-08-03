fb82fbe108cfc414c9c96d52574173001f306cf2
sun2525 <ghd7673@naver.com>
2022-08-03 13:50:03 +0900

    add search

--- post-upload/app.py
+++ post-upload/app.py
@@ -8,6 +8,8 @@ db = client.dbsparta_plus_week1
 
 from datetime import datetime
 
+from bson.json_util import dumps
+
 @app.route('/')
 def home():
     return render_template('post_upload.html')
@@ -16,11 +18,21 @@ def home():
 def main():
     return render_template('main_test.html')
 
-#데이터베이스에 저장된 게시글을 전송해 main_test.html파일에서 보여줌
+#검색 기능
 @app.route('/view', methods=['GET'])
 def show_diary():
     diaries = list(db.diary.find({}, {'_id': False}))
     return jsonify({'all_diary': diaries})
+
+#데이터베이스에 저장된 게시글을 전송해 main_test.html파일에서 보여줌
+@app.route('/search', methods=['POST'])
+def search_show():
+    find_name = request.form.get('find_name_give', False)
+    search_name = request.form.get('search_name_give', False)
+    print(find_name,search_name)
+
+    diaries = list(db.diary.find({find_name: {'$regex':search_name}}))
+    return jsonify({'all_diary': dumps(diaries)})
 
 #사용자로부터 게시글 데이터가 들어오면 데이터베이스에 저장
 @app.route('/posting', methods=['POST'])
